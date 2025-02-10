599c14ac7af497a06effc7c5f38210a333c19a46
Santiago Toro <santoloza@hotmail.com>
2025-02-10 18:18:17 -0500

    editing and import routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>hi world!</h1>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/auth" />} />
+          <Route path="/auth" component={AuthPage} />
+          <Route path='/events' component={null} />
+          <Route path='/bookings' component={null} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+
 }
 
 export default App;
