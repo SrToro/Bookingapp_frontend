54e2dd4b2e3c83b2be9c23ca4aaa4705c6716c22
Santiago Toro <santoloza@hotmail.com>
2025-02-11 14:38:27 -0500

    redirecting to the pages booking events and users correctly

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+import UserPage from './pages/User';
 
 import './App.css';
 
@@ -12,8 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/auth" />} />
         <Route path="/auth" element={<AuthPage/>} />
-        <Route path='/events' element={null} />
-        <Route path='/bookings' element={null} />
+        <Route path='/user' element={<UserPage/>} />
+        <Route path='/events' element={<EventsPage/>} />
+        <Route path='/bookings' element={<BookingsPage/>} />
       </Routes>
     </BrowserRouter>
   );
