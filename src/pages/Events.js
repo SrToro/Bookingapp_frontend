13499a8232ddc648dfb376cf3bd632fa48eccbcd
Santiago Toro <santoloza@hotmail.com>
2025-02-27 21:21:19 -0500

    configure css to the backdrop for the modal

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import Modal from '../components/Modal/Modal'
+import Backdrop from "../components/Backdrop/Backdrop";
 
 import './Events.css';
 
@@ -8,6 +9,7 @@ class EventsPage extends Component {
     render() {
         return (
             <React.Fragment>
+                <Backdrop/>
                 <Modal title="Add Event" canCancel canConfirm>
                     <p>Modal content</p>
                 </Modal>
